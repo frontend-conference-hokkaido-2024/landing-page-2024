68d7a33596d9de75aeb2ffd8ba8815c2fea601d7
618knot <publicac.clt618knot@gmail.com>
2024-04-07 23:33:08 +0900

    指摘修正

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
-import LinkMenu from "@/components/LinkMenu/LinkMenu";
-import SpMenu from "@/components/SpMenu/SpMenu";
+import LinkMenu from "@/components/LinkMenu";
+import SpMenu from "@/components/SpMenu";
 
 export default function Home() {
   return (
@@ -10,7 +10,7 @@ export default function Home() {
       </div>
       <main className="w-full max-w-[500px] md:w-[360px] h-dvh mx-auto place-items-center">
         {/* ここにSP用のタグとかを書いていく */}
-        <SpMenu />
+        <SpMenu menu={<LinkMenu />}/>
       </main>
       <div className="fixed top-0 right-0 hidden md:block md:w-[calc((100%_-_360px)/4*1)] lg:w-[calc((100%_-_360px)/2)] h-dvh bg-fuchsia-700">
       </div>

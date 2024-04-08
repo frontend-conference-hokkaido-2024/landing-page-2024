70b51f69c76e3a82faeaa34db124cd2117a2b2cc
kengo2003 <saitokengo2003@gmail.com>
2024-04-08 22:52:58 +0900

    PC画面の実装

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,14 +3,19 @@ import TopSection from "@/components/Top/page";
 export default function Home() {
   return (
     <>
-      <div className="fixed top-0 left-0 hidden md:block md:w-[calc((100%_-_360px)/4*3)] lg:w-[calc((100%_-_360px)/2)] h-dvh bg-fuchsia-700">
-        {/* 左側エリア */}
+      <div className="lg:hidden">
+        <div className="fixed top-0 left-0 hidden md:block md:w-[calc((100%_-_360px)/4*3)] lg:w-[calc((100%_-_360px)/2)] h-dvh bg-fuchsia-700">
+          {/* 左側エリア */}
+        </div>
+        <main className="w-full max-w-[500px] md:w-[360px] h-dvh mx-auto place-items-center">
+          {/* ここにSP用のタグとかを書いていく */}
+          <TopSection />
+        </main>
+        <div className="fixed top-0 right-0 hidden md:block md:w-[calc((100%_-_360px)/4*1)] lg:w-[calc((100%_-_360px)/2)] h-dvh bg-fuchsia-700"></div>
       </div>
-      <main className="w-full max-w-[500px] md:w-[360px] h-dvh mx-auto place-items-center">
-        {/* ここにSP用のタグとかを書いていく */}
+      <div className="hidden lg:block">
         <TopSection />
-      </main>
-      <div className="fixed top-0 right-0 hidden md:block md:w-[calc((100%_-_360px)/4*1)] lg:w-[calc((100%_-_360px)/2)] h-dvh bg-fuchsia-700"></div>
+      </div>
     </>
   );
 }
--- src/components/Top/page.tsx
+++ src/components/Top/page.tsx
@@ -9,38 +9,54 @@ const page = () => {
       <Image
         src={"/Image/jingisukan.jpg"}
         alt={"ジンギスカンの画像です"}
-        layout="fill"
-        className="z-[-1] hidden lg:block absolute opacity-50"
+        width={1000}
+        height={500}
+        className="z-[-1] lg:w-full hidden lg:block absolute opacity-40"
       />
-      <div className="text-black z-1 px-5 pt-20 lg:pt-72">
-        <h1 className="text-4xl font-bold lg:text-center lg:text-5xl">
+      <div className="text-black z-1 px-5 pt-20 lg:px-20 lg:pt-72">
+        <h1 className="text-4xl font-bold lg:text-center lg:py-4 lg:text-5xl">
           Frontend <br className="lg:hidden" />
           Conference <br className="lg:hidden" />
           Hokkaido <br className="lg:hidden" />
           2024（仮）
         </h1>
-        <p className="my-2 lg:my-8 text-xl text-slate-500">
+        <p className="my-2 lg:my-8 text-xl text-slate-600">
           フロントエンドカンファレンス
           <br className="lg:hidden" />
           北海道2024
         </p>
         <ul className="lg:flex lg:mb-8">
           <li className="flex mr-6">
-            <p className="mr-10 text-slate-500">Date</p>
-            <p className="font-bold">2024.08.24</p>
+            <p className="mr-10 text-slate-600">Date</p>
+            <p className="font-bold text-xl">2024.08.24</p>
           </li>
           <li className="flex mr-6">
-            <p className="mr-10 text-slate-500">Time</p>
-            <p className="font-bold">10:00-18:30</p>
+            <p className="mr-10 text-slate-600">Time</p>
+            <p className="font-bold text-xl">10:00-18:30</p>
           </li>
           <li className="flex">
-            <p className="mr-4 text-slate-500">Location</p>
-            <p className="font-bold">Deep Tech CORE</p>
+            <p className="mr-4 text-slate-600">Location</p>
+            <p className="font-bold text-xl">Deep Tech CORE</p>
           </li>
         </ul>
         <div className="hidden lg:flex">
-          <p className="mr-10">Icon</p>
-          <p>Icon</p>
+          <button className="mr-8">
+            <Image
+              src={"/Image/note-icon.png"}
+              width={35}
+              height={35}
+              alt="noteのアイコンです"
+              className="border-2 border-slate-200"
+            />
+          </button>
+          <button>
+            <Image
+              src={"/Image/mail-icon.png"}
+              width={35}
+              height={35}
+              alt="mailのアイコンです"
+            />
+          </button>
         </div>
         <Link href="#" className="lg:hidden">
           <button className=" bg-slate-900 py-2 mt-2 w-full rounded-lg text-white">

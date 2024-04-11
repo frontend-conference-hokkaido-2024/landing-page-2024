de294197bfaa7d51e7adff1f168ec2b9ccfd5858
kengo2003 <saitokengo2003@gmail.com>
2024-04-11 22:59:24 +0900

    footerの実装

---
+++ src/app/components/Page.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Page = () => {
+  return (
+    <main className="w-full h-52 bg-slate-800 py-8">
+      <div className="text-slate-300  text-center [&_p]:py-4">
+        <p>フロントエンドカンファレンス北海道2024</p>
+        <p>
+          @2024 フロントエンドカンファレンス
+          <br />
+          北海道実行委員会
+        </p>
+      </div>
+    </main>
+  );
+};
+
+export default Page;

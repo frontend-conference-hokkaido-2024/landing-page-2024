82ca534c00844a0f5970eb90dddeb0c0c699e414
kengo2003 <saitokengo2003@gmail.com>
2024-04-15 19:40:44 +0900

    mainタグをfooterタグに変更

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -1,6 +1,6 @@
 const Footer = () => {
   return (
-    <main className="w-full h-52 bg-slate-800 py-8">
+    <footer className="w-full h-52 bg-slate-800 py-8">
       <div className="text-slate-300 text-center [&_p]:py-4">
         <p>フロントエンドカンファレンス北海道2024</p>
         <p>
@@ -9,7 +9,7 @@ const Footer = () => {
           北海道実行委員会
         </p>
       </div>
-    </main>
+    </footer>
   );
 };
 

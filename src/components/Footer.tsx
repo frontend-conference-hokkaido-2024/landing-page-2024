431f2af32a093883f465c8e67a574bc7de0f20dd
kengo2003 <saitokengo2003@gmail.com>
2024-04-17 22:07:23 +0900

    カラー変更

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,6 +1,6 @@
 const Footer = () => {
   return (
-    <footer className="w-full bg-slate-800 py-8">
+    <footer className="w-full bg-slate-900 py-8">
       <div className="text-white text-center [&_p]:py-4 text-sm">
         <p>フロントエンドカンファレンス北海道2024</p>
         <p>

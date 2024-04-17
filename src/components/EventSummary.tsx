fd5a405b6edd33a22010c193def097e99d1437a1
髙橋紀美 <xvipea0@gmail.com>
2024-04-18 03:38:48 +0900

    イベント詳細を追加

--- src/components/EventSummary.tsx
+++ src/components/EventSummary.tsx
@@ -1,9 +1,19 @@
-
 export default function EventSummary() {
     return(
-        <div className="text-center">
-            <h1 className="text-3xl font-extrabold">イベント概要</h1>
-            <h3 className="font-semibold text-2xl">フロントエンドカンファレンス北海道2024を開催します！</h3>
-            </div>
+        <div style={{ marginLeft: '20px', marginRight: '20px', textAlign: 'center' }}>
+            <h1 className="text-3xl font-extrabold m-8">イベント概要</h1>
+            <h3 className="font-semibold text-2xl break-words mb-4">フロントエンドカンファレンス北海道2024を開催します！</h3>
+            <p className="break-words text-left">2024年8月24日（土）に北海道札幌市で開催する、フロントエンド領域に関心のある参加者を対象とした技術イベントです。
+                <br />
+                <br />
+                2023年11月に沖縄で開催された、フロントエンドカンファレンス沖縄をリスペクトしたカンファレンスでもあります。
+                <br />
+                <br />
+                札幌を中心に、全国のプロダクト開発に関わるエンジニアとデザイナーが交わる場を作ることを目指し、HTML、CSS、JavaScript、UI、UX、デザイン、アクセシビリティ、SPA、PWAなど、Webに携わるすべてのフロントエンド領域に関心のある方を対象としています。
+                <br />
+                <br />
+                札幌市内での現地開催のほか、オンライン配信（アーカイブあり）も予定しております。参加チケットは販売開始次第、公式Xアカウント(@fec_hokkaido)でお知らせします。ぜひフォローして、通知をオンにしてお待ちください。
+            </p>
+        </div>
     );
 }

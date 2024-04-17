35890fc58e5ab528725d571e129f3a915995257e
髙橋紀美 <xvipea0@gmail.com>
2024-04-18 02:19:41 +0900

    Event Summary コンポーネントを作成 + Homeに表示する

---
+++ src/components/EventSummary.tsx
@@ -0,0 +1,9 @@
+
+export default function EventSummary() {
+    return(
+        <div className="text-center">
+            <h1 className="text-3xl font-extrabold">イベント概要</h1>
+            <h3 className="font-semibold text-2xl">フロントエンドカンファレンス北海道2024を開催します！</h3>
+            </div>
+    );
+}

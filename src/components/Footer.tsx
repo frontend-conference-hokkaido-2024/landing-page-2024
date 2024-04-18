e05f6ecf678acfb8a7e37aa2d36ce9fd3923d82e
kengo2003 <saitokengo2003@gmail.com>
2024-04-18 22:14:57 +0900

    プライバシーポリシーの設置

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,13 +1,18 @@
+import Link from "next/link";
+
 const Footer = () => {
   return (
     <footer className="w-full bg-slate-900 py-8">
-      <div className="text-white text-center [&_p]:py-4 text-sm">
-        <p>フロントエンドカンファレンス北海道2024</p>
-        <p>
+      <div className="text-white text-center text-sm">
+        <p className="py-4">フロントエンドカンファレンス北海道2024</p>
+        <p className="py-4">
           @2024 フロントエンドカンファレンス
           <br />
           北海道実行委員会
         </p>
+        <Link href="#" className="py-4 inline-block">
+          プライバシーポリシー
+        </Link>
       </div>
     </footer>
   );

91136a5f295fde5133ba655ba4e20eaa03b2bc6a
kengo2003 <saitokengo2003@gmail.com>
2024-04-22 18:37:10 +0900

    gitignoreの修正

--- src/components/EventOverView.tsx
+++ src/components/EventOverView.tsx
@@ -1,6 +1,7 @@
-import Link from "next/link";
-import { Button } from "@/components/ui/button";
 import Image from "next/image";
+import Link from "next/link";
+
+import { Button } from "@/components/ui/button";
 
 const EventOverView = () => {
   return (

41422209ba24b91b2f21c8eb582b6fbb87f83ecf
618knot <publicac.clt618knot@gmail.com>
2024-04-24 00:27:11 +0900

    fadeIn追加

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -67,10 +67,15 @@ const config = {
           from: { height: "var(--radix-accordion-content-height)" },
           to: { height: "0" },
         },
+        fadeIn: {
+          "0%": { opacity: "0" },
+          "100%": { opacity: "1" },
+        },
       },
       animation: {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
+        "fadeIn": "fadeIn 0.3s ease-in-out",
       },
     },
   },

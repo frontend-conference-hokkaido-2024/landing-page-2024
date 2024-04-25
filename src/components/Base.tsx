8f35d80e0c91847bfe46977c1b4af7bdebeb80a2
618knot <publicac.clt618knot@gmail.com>
2024-04-25 16:19:39 +0900

    コンポーネント分離

---
+++ src/components/Base.tsx
@@ -0,0 +1,25 @@
+import LinkMenu from "@/components/LinkMenu";
+import SpMenu from "@/components/SpMenu";
+import { ReactNode } from "react";
+
+type BaseProps = {
+    children?: ReactNode
+}
+
+export default function Base({ children }: BaseProps) {
+  return (
+    <>
+      <div className="fixed top-0 left-0 hidden md:block md:w-[calc((100%_-_360px)/4*3)] lg:w-[calc((100%_-_360px)/2)] h-dvh bg-fuchsia-700">
+        {/* 左側エリア */}
+        <LinkMenu />
+      </div>
+      <main className="w-full max-w-[500px] md:w-[360px] h-dvh mx-auto place-items-center">
+        {/* ここにSP用のタグとかを書いていく */}
+        <SpMenu menu={<LinkMenu />}/>
+        {children}
+      </main>
+      <div className="fixed top-0 right-0 hidden md:block md:w-[calc((100%_-_360px)/4*1)] lg:w-[calc((100%_-_360px)/2)] h-dvh bg-fuchsia-700">
+      </div>
+    </>
+  );
+}

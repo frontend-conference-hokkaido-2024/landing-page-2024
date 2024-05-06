37958e27fac2aee20bfc63108b26cc06725cb423
618knot <publicac.clt618knot@gmail.com>
2024-05-06 13:43:59 +0900

    p 修正

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,7 @@ export default function Home() {
   return (
     <Base>
       <div className="w-full flex flex-col gap-24">
-        <main className="flex flex-col gap-24 p-12">
+        <main className="flex flex-col gap-24 lg:px-12">
           <EventOverView />
           <EventSummary />
           <AccessInfo />

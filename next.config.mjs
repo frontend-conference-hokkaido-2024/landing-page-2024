d7cf64d1b1a4549bb3c8617fd076fa24db54d0df
髙橋紀美 <xvipea0@gmail.com>
2024-05-06 15:08:21 +0900

    next.config.jsを更新してfortee.jpのフロントエンド北海道関連ページから画像を取得する

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+
+const nextConfig = {
+    images: {
+        remotePatterns: [
+          {
+            protocol: "https",
+            hostname: "fortee.jp",
+            port: "",
+            pathname: "/files/frontend-conf-hokkaido-2024/**/*.{jpg,png}",
+          },
+        ],
+      },
+};
 
 export default nextConfig;

273b5c7c0c707aa96741e01adf3a0478fc0b4e29
618knot <publicac.clt618knot@gmail.com>
2024-05-06 12:28:26 +0900

    grid -> table, 日本語に統一

--- src/components/EventOverView.tsx
+++ src/components/EventOverView.tsx
@@ -1,28 +1,46 @@
 import Image from "next/image";
+import TopVisual from "./TopVisual";
+
+
 // import Link from "next/link";
 
 // import { Button } from "@/components/ui/button";
 
 const EventOverView = () => {
   return (
-    <div className="mt-32">
-      <Image
-        src={"/images/note-image-in-contents.png"}
-        width={600}
-        height={500}
-        alt="Frontend Conference HOKKAIDO 2024"
-      />
-      <div className="text-black px-5">
-        <div className="grid grid-cols-4 text-sm">
-          <p className="text-slate-500">Date</p>
-          <p className="font-semibold col-span-3">2024.08.24</p>
-
-          <p className="text-slate-500">Time</p>
-          <p className="font-semibold col-span-3">10:00-18:30</p>
-
-          <p className="text-slate-500">Location</p>
-          <p className="font-semibold col-span-3">Deep Tech CORE SAPPORO</p>
-        </div>
+    <div className="">
+      <div className="w-full p-4">
+        <TopVisual />
+      </div>
+      <div className="text-black px-5 mx-auto flex flex-col items-center mt-4">
+        <table>
+            <tbody>
+              <tr>
+                <td className="w-1/4">
+                  <p className="text-slate-500 lg:text-xl">開催日時</p>
+                </td>
+                <td className="w-2/4">
+                  <p className="font-semibold col-span-3 lg:text-xl">2024.08.24(土) 10:00-18:30</p>
+                </td>
+              </tr>
+              <tr>
+                <td>
+                  <p className="text-slate-500 lg:text-xl">開催時間</p>
+                </td>
+                <td>
+                  <p className="font-semibold col-span-3 lg:text-xl">10:00-18:30</p>
+                </td>
+              </tr>
+              <tr>
+                <td>
+                  <p className="text-slate-500 lg:text-xl">会場</p>
+                </td>
+                <td>
+                  <p className="font-semibold col-span-3 lg:text-xl">Deep Tech CORE</p>
+                </td>
+              </tr>
+            </tbody>
+          </table>
         {/* <Button asChild className="mt-2 w-full text-sm font-bold">
           <Link href="#">参加申し込み</Link>
         </Button> */}

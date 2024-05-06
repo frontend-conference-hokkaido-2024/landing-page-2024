0fee72e4e848c8c6901a48495cca6d7c965652ec
髙橋紀美 <xvipea0@gmail.com>
2024-05-07 01:00:30 +0900

    nextConfigを編集
    画像は最適化しない。

--- next.config.mjs
+++ next.config.mjs
@@ -1,17 +1,17 @@
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {
-    // output: "export",
+    output: "export",
     images: {
-        remotePatterns: [
-          {
-            protocol: "https",
-            hostname: "fortee.jp",
-            port: "",
-            pathname: "/files/frontend-conf-hokkaido-2024/**/*.{jpg,png}",
-          },
-        ],
-      }
+      unoptimized: true,
+      remotePatterns: [
+        {
+          protocol: "https",
+          hostname: "fortee.jp",
+          port: "",
+          pathname: "/files/frontend-conf-hokkaido-2024/**/*.{jpg,png}",
+        },
+      ],
+  }
 };
-
 export default nextConfig;

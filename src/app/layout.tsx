35f28c8ac21a7b6cb2aebee7d468a94eba3bc1b3
618knot <publicac.clt618knot@gmail.com>
2024-05-06 16:07:58 +0900

    notosansjp追加

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,12 @@
 import clsx from "clsx";
-import { Inter } from "next/font/google";
+import { Inter, Noto_Sans_JP } from "next/font/google";
 
 import type { Metadata } from "next";
 
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
+const NotoSansJP = Noto_Sans_JP({ subsets: ["latin"] });
 
 const title: string = "フロントエンドカンファレンス北海道2024";
 const description: string = "フロントエンド領域のエンジニア・デザイナー向けの技術カンファレンス「フロントエンドカンファレンス北海道2024」を2024年8月24日(土)に札幌で開催";
@@ -31,7 +32,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ja">
-      <body className={clsx("relative", inter.className)}>
+      <body className={clsx("relative")} style={{fontFamily: `${inter.style.fontFamily}, ${NotoSansJP.style.fontFamily}`}}>
         <div></div>{/*背景に何か要素をつけたいのであれば...*/}
         {children}
       </body>

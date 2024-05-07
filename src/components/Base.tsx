be43c5d12129e8d5af64e31ad8ec83b2d125c8ec
618knot <publicac.clt618knot@gmail.com>
2024-05-07 15:03:45 +0900

    メニューを調整

--- src/components/Base.tsx
+++ src/components/Base.tsx
@@ -13,9 +13,9 @@ type BaseProps = {
 export default function Base({ children }: BaseProps) {
   return (
     <>
-      <div className="fixed top-0 left-0 hidden md:block md:w-[calc((100%_-_360px)/4*3)] lg:w-[calc((100%_-_768px)/2)] h-dvh bg-gradient-to-b from-[rgba(66,102,245,0.5)] to-[rgba(245,66,182,0.5)]">
+      <div className="fixed top-0 left-0 hidden md:block md:w-[calc((100%_-_360px)/4*3)] lg:w-[calc((100%_-_768px)/2)] h-dvh bg-gradient-to-b from-[rgba(66,102,245,0.5)] to-[rgba(245,66,182,0.5)] py-72 px-28">
         {/* 左側エリア */}
-        <Card className="flex items-center justify-center w-56 h-56 rounded-md mx-auto mt-20 bg-slate-50">
+        <Card className="flex items-center justify-center w-full h-full rounded-md bg-slate-50">
           <LinkMenu textClassName="text-[#2a0252]"/>
         </Card>
       </div>
@@ -24,8 +24,8 @@ export default function Base({ children }: BaseProps) {
         <SpMenu menu={<LinkMenu />}/>
         {children}
       </div>
-      <div className="fixed top-0 right-0 hidden md:block md:w-[calc((100%_-_360px)/4*1)] lg:w-[calc((100%_-_768px)/2)] h-dvh bg-gradient-to-b from-[rgba(66,102,245,0.5)] to-[rgba(245,66,182,0.5)]">
-        <Card className="flex items-center justify-center w-64 h-16 rounded-md mx-auto mt-20 bg-slate-50">
+      <div className="fixed top-0 right-0 hidden md:block md:w-[calc((100%_-_360px)/4*1)] lg:w-[calc((100%_-_768px)/2)] h-dvh bg-gradient-to-b from-[rgba(66,102,245,0.5)] to-[rgba(245,66,182,0.5)] py-96 px-28">
+        <Card className="flex items-center justify-center w-full h-full rounded-md mx-auto bg-slate-50">
           <LinkButton href={{
             host: "fortee.jp/",
             pathname: "/frontend-conf-hokkaido-2024/sponsor/form"

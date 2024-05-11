6cd4f83e2f5b0c3c8022739691ca94ea089ea80f
618knot <publicac.clt618knot@gmail.com>
2024-05-11 11:30:54 +0900

    urlオブジェクトにprotocol追加

--- src/components/LinkMenu.tsx
+++ src/components/LinkMenu.tsx
@@ -14,10 +14,12 @@ export default function LinkMenu({ className, textClassName }: LinkMenuProps) {
     return ( 
         <div className={clsx("bg-transparent flex flex-col gap-2 w-32", className)}>
             <LinkButton href={{
+                protocol: "https://",
                 host: "fortee.jp",
                 pathname: "/frontend-conf-hokkaido-2024/proposal/all",
             }}><span className={clsx("flex", textClassName)}>TALKS<ExternalLinkIcon /></span></LinkButton>
             <LinkButton href={{
+                protocol: "https://",
                 host: "fortee.jp",
                 pathname: "/frontend-conf-hokkaido-2024/timetable",
             }}><span className={clsx("flex", textClassName)}>TIMETABLE<ExternalLinkIcon /></span></LinkButton>
@@ -26,15 +28,18 @@ export default function LinkMenu({ className, textClassName }: LinkMenuProps) {
             }}
             disabled><span className={clsx("font-black text-lg pl-4 italic text-[#2a025286]")}>JOB BOARD</span></LinkButton>
             <LinkButton href={{
+                protocol: "https://",
                 host: "www.notion.so",
                 pathname: "/cf184497a6414a97aea49c1f1b2f5b5f",
                 query: { pvs: 4 },
             }}><span className={clsx("flex", textClassName)}>POLICY<ExternalLinkIcon /></span></LinkButton>
             <LinkButton href={{
+                protocol: "https://",
                 host: "note.com",
                 pathname: "/fec_hokkaido",
             }}><span className={clsx("flex", textClassName)}>STAFF BLOG<ExternalLinkIcon /></span></LinkButton>
             <LinkButton href={{
+                protocol: "https://",
                 host: "twitter.com",
                 pathname: "/fec_hokkaido",
             }}><span className={clsx("flex", textClassName)}>OFFICIAL 𝕏<ExternalLinkIcon /></span></LinkButton>

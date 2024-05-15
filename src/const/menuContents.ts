9cfd46c77ba580292db961723457b908e05d4721
618knot <publicac.clt618knot@gmail.com>
2024-05-15 09:27:43 +0900

    menuContetnsの定数を追加

---
+++ src/const/menuContents.ts
@@ -0,0 +1,28 @@
+import type { MenuProps } from "menu";
+
+export const MENU_CONTENTS: MenuProps = [
+  {
+    title: "TALKS",
+    href: "https://fortee.jp/frontend-conf-hokkaido-2024/proposal/all",
+  },
+  {
+    title: "TIMETABLE",
+    href: "https://fortee.jp/frontend-conf-hokkaido-2024/timetable",
+  },
+  {
+    title: "JOB BOARD",
+    href: "/jobboard",
+  },
+  {
+    title: "POLICY",
+    href: "https://aback-jasmine-06b.notion.site/cf184497a6414a97aea49c1f1b2f5b5f",
+  },
+  {
+    title: "STAFF BLOG",
+    href: "https://note.com/fec_hokkaido",
+  },
+  {
+    title: "OFFICIAL 𝕏",
+    href: "https://twitter.com/fec_hokkaido"
+  }
+];

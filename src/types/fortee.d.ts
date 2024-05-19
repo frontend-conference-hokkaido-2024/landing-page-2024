7c3f97ce6668a0378df142f785ac34b796ab3349
髙橋紀美 <xvipea0@gmail.com>
2024-05-19 15:32:22 +0900

    コメントの追加

--- src/types/fortee.d.ts
+++ src/types/fortee.d.ts
@@ -1,4 +1,5 @@
-// Forteeに依存してもいなくても、より汎用的な型定義の方が適している
+// APIなどに使う型情報だけであればfortee.d.tsなどに含める方が良い
+// Forteeに依存が前提になっているので、より汎用的な型定義の方が適している
 declare module "fortee" {
     type StaffCategory = "core";
 

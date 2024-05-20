7f4c72470adaf2b6e9a96d616fe5fe8d67d8d46e
髙橋紀美 <xvipea0@gmail.com>
2024-05-20 11:11:22 +0900

    インデントの調整

--- src/app/@avataiList/page.tsx
+++ src/app/@avataiList/page.tsx
@@ -8,18 +8,18 @@ export default async function Page() {
 		throw new Error("環境変数FORTEE_API_HOSTが設定されていません");
 	}
 
-  const response = await fetch(`${process.env.FORTEE_API_HOST}/staff`, {
+	const response = await fetch(`${process.env.FORTEE_API_HOST}/staff`, {
 		// Next.jsのcache戦略によりfetch結果がキャッシュされるので無効化（ビルド間でのキャッシュ共有を避けるため）
     cache: "no-cache",
-  });
+});
 
-  if (!response.ok) {
+if (!response.ok) {
     throw new Error(
-      `データ取得に失敗しました.\n  HTTPステータス: ${response.status}`
+		`データ取得に失敗しました.\n  HTTPステータス: ${response.status}`
     );
-  }
+}
 
-  const data = await response.json() as {
+const data = await response.json() as {
 		staff: {
 			" core_staff": Person[];
 		};
@@ -36,22 +36,22 @@ export default async function Page() {
 		) 
 	} as StaffData; // typedMapを使い型をつける方が良いが, 今回はas句で強制キャスト
 
-  const people: Person[] = cleanedData.staff.core_staff;
+const people: Person[] = cleanedData.staff.core_staff;
   // peopleがnullまたは空の配列の場合は何も表示しない
 
 
-  return (
+return (
     <div className="mx-5 mb-12 text-center">
-      <h1 className="text-3xl font-extrabold mb-8">{"コアスタッフ"}</h1>
-      <div className="grid grid-cols-2 lg:grid-cols-3">
-        {people.length === 0 ? (
-					<div>データがありません</div>
-				) : (
-					people.map((person) => (
-						<Avatar key={person.id} person={person} />
-					))
-				)}
-      </div>
-    </div>
-  );
+		<h1 className="text-3xl font-extrabold mb-8">{"コアスタッフ"}</h1>
+		<div className="grid grid-cols-2 lg:grid-cols-3">
+			{people.length === 0 ? (
+			<div>データがありません</div>
+		) : (
+			people.map((person) => (
+			<Avatar key={person.id} person={person} />
+		))
+		)}
+		</div>
+		</div>
+		);
 }

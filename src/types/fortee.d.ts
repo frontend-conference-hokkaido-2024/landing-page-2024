262981b539cdcba5fd6cd20245b5010e73877d66
髙橋紀美 <xvipea0@gmail.com>
2024-05-21 02:49:20 +0900

    Fortee型定義を追加

---
+++ src/types/fortee.d.ts
@@ -0,0 +1,17 @@
+
+declare module "fortee" {
+    type StaffCategory = "core";
+
+    type StaffData = {
+        staff: {
+        core_staff: Person[];
+    };
+    };
+
+    type Person = {
+        id: string;
+        name: string;
+        url: string;
+        avatar_url: string;
+    };
+}

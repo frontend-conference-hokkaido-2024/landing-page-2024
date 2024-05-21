ff8c4f7f5b055c8a416baf8c98897b73f5e22568
618knot <publicac.clt618knot@gmail.com>
2024-05-21 18:40:05 +0900

    / -> /2024 にリダイレクトするように

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,5 @@
-import AccessInfo from "@/components/AccessInfo";
-import Base from "@/components/Base";
-import EventOverView from "@/components/EventOverView";
-import EventSummary from "@/components/EventSummary";
-import Footer from "@/components/Footer";
+import { redirect } from "next/navigation";
 
-export default function Home() {
-  return (
-    <Base>
-      <div className="w-full flex flex-col gap-24">
-        <main className="flex flex-col gap-24 lg:px-12">
-          <EventOverView />
-          <EventSummary />
-          <AccessInfo />
-        </main>
-        <Footer />
-      </div>
-    </Base>
-  );
+export default function Root() {
+  redirect("/2024");
 }

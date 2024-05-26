f7b00221a81b078b33b330493e66e42684dda151
Nishimura Wataru <wataru.nishimura@irenkakotan.co.jp>
2024-05-26 12:15:36 +0900

    Revert "共通ボタンコンポーネントの実装 (#100)"
    This reverts commit d54c7f26ed2fc903ae6d8f3eea6e94847f6ddd05.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,15 @@
 import AccessInfo from "@/components/AccessInfo";
 import EventOverView from "@/components/EventOverView";
 import EventSummary from "@/components/EventSummary";
-import CommonButton from "@/components/ui/CommonButton";
 
 export default function Home() {
   return (
-    <div className="w-full flex flex-col gap-24">
-      <main className="flex flex-col gap-24 lg:px-12">
-        <EventOverView />
-        <CommonButton
-          background={"bg-gradient-to-r from-pink-500 to-blue-500 "}
-          lefticon={"Images/Icon/icon_crab.png"}
-          righticon={"Images/Icon/icon_crab.png"}
-        >
-          チケット購入
-        </CommonButton>
-        <EventSummary />
-        <AccessInfo />
-      </main>
-    </div>
+      <div className="w-full flex flex-col gap-24">
+        <main className="flex flex-col gap-24 lg:px-12">
+          <EventOverView />
+          <EventSummary />
+          <AccessInfo />
+        </main>
+      </div>
   );
 }

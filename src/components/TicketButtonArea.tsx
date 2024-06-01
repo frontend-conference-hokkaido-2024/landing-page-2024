cba0eb886d224893efc28bd376be92f1292a3697
618knot <knot618@gmail.com>
2024-06-01 23:38:08 +0900

    link先追加

--- src/components/TicketButtonArea.tsx
+++ src/components/TicketButtonArea.tsx
@@ -1,15 +1,21 @@
+"use client";
+
 import { Ticket } from "lucide-react";
+import Link from "next/link";
 
 import CommonButton from "./ui/CommonButton";
 
 export default function TicketButtonArea() {
   return (
     <div className="flex flex-col gap-1 items-center w-full">
-      <CommonButton
-        background="bg-gradient-to-r from-pink-500 to-blue-500"
-        lefticon={<Ticket />}>
-        チケット購入
-      </CommonButton>
+      <Link href="https://example.com" className="w-full" target="_blank" rel="noopener noreferrer">
+        <CommonButton
+          background="bg-gradient-to-r from-pink-500 to-blue-500"
+          lefticon={<Ticket />}
+        >
+          チケット購入
+        </CommonButton>
+      </Link>
       <p className="text-caution text-xs font-bold">※オンライン視聴の場合もチケット購入が必要です</p>
     </div>
   );

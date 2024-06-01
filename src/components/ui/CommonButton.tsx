d64f13f0260898df3412e8bd114a477a4f3960ea
618knot <knot618@gmail.com>
2024-06-02 00:33:48 +0900

    class名の適用

--- src/components/ui/CommonButton.tsx
+++ src/components/ui/CommonButton.tsx
@@ -14,13 +14,15 @@ export default function CommonButton({
   lefticon,
   righticon,
   children,
+  className,
   ...props
 }: buttonProps) {
   return (
     <button
       className={clsx(
         "w-full text-white rounded-full px-4 py-2.5 flex items-center justify-center hover:bg-opacity-80 transition duration-200 focus:outline-none",
-        background
+        background,
+        className
       )}
       {...props}
     >

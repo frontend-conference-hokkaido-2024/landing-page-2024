3595c5d324fc88e283f1403cabd829cac5080b4b
618knot <knot618@gmail.com>
2024-06-05 02:55:23 +0900

    画面内移動の手段を変更

--- src/components/menu/MenuList.tsx
+++ src/components/menu/MenuList.tsx
@@ -10,13 +10,6 @@ import { Separator } from "../ui/separator";
 
 import type { ButtonProps } from "../ui/button";
 
-const scrollToSection = (elementId: string) => {
-  const element = document.getElementById(elementId);
-  if (element) {
-    element.scrollIntoView({ behavior: "smooth" });
-  }
-};
-
 type MenuListProps = {
   color?: string;
 }
@@ -31,13 +24,33 @@ export default function MenuList({ color = "white" }: MenuListProps) {
 
   return (
     <div className="flex flex-col items-start w-fit">
-      <MenuButton onClick={() => scrollToSection("event-over-view")}>開催概要</MenuButton>
-      <MenuButton onClick={() => scrollToSection("access")}>会場アクセス</MenuButton>
-      <MenuButton onClick={() => scrollToSection("online-streaming")}>オンライン配信</MenuButton>
-      <MenuButton onClick={() => scrollToSection("sponsor")}>スポンサー</MenuButton>
-      <MenuButton onClick={() => scrollToSection("core-staff")}>スタッフ</MenuButton>
+      <MenuButton>
+        <a href="#event-over-view">
+          開催概要
+        </a>
+      </MenuButton>
+      <MenuButton>
+        <a href="#access">
+          会場アクセス
+        </a>
+      </MenuButton>
+      <MenuButton>
+        <a href="#online-streaming">
+          オンライン配信
+        </a>
+      </MenuButton>
+      <MenuButton>
+        <a href="#sponsor">
+          スポンサー
+        </a>
+      </MenuButton>
+      <MenuButton>
+        <a href="#core-staff">
+          スタッフ
+        </a>
+      </MenuButton>
       <MenuButton asChild>
-        <Link href={{pathname: "job-board"}}>
+        <Link href={{ pathname: "job-board" }}>
           ジョブボード
         </Link>
       </MenuButton>
@@ -51,14 +64,16 @@ export default function MenuList({ color = "white" }: MenuListProps) {
       </MenuButton>
       <MenuButton asChild>
         <Link href="https://note.com/fec_hokkaido" target="_blank" rel="noopener noreferrer">
-          スタッフブログ</Link>
+          スタッフブログ
+        </Link>
       </MenuButton>
 
       <Separator className={clsx("border w-full my-2", borderColor)} />
 
       <MenuButton asChild>
         <Link href="https://aback-jasmine-06b.notion.site/1f09ab38bf594ab689b244fec8f1ac46" target="_blank" rel="noopener noreferrer">
-          行動規範</Link>
+          行動規範
+        </Link>
       </MenuButton>
       <MenuButton asChild>
         <Link href="https://www.local.or.jp/about/tokushohou" target="_blank" rel="noopener noreferrer">

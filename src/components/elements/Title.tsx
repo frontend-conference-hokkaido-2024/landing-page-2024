c329fbeb9fc5dc8ff17689887293b902d4988c9c
髙橋紀美 <xvipea0@gmail.com>
2024-06-07 00:52:54 +0900

    Titleコンポーネントにh1タグを持たせる

--- src/components/elements/Title.tsx
+++ src/components/elements/Title.tsx
@@ -5,12 +5,15 @@ import type { ReactNode } from "react";
 type TitleProps = {
   children: ReactNode;
   icon: ReactNode;
-}
+};
 
-export default function Title({children, icon}: TitleProps) {
-  return(
-    <div className="flex items-center justify-center gap-2 font-bold text-3xl tracking-wider">
-      {icon}{children}
-    </div>
+export default function Title({ children, icon }: TitleProps) {
+  return (
+    <h1 className="font-extrabold mb-8">
+      <div className="flex items-center justify-center gap-2 font-bold text-3xl tracking-wider">
+        {icon}
+        {children}
+      </div>
+    </h1>
   );
 }

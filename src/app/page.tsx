dd1f2a5249eee7b2c6fef3f738faad5f94abb50b
618knot <knot618@gmail.com>
2024-06-08 21:22:55 +0900

    2重mainタグの解消・padding修正

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,25 +8,9 @@ import CommonButton from "@/components/ui/CommonButton";
 export default function Home() {
   return (
     <div className="w-full flex flex-col gap-24">
-      <main className="flex flex-col gap-24 lg:px-12">
-        <EventOverView />
-        <CommonButton
-          background={"bg-gradient-to-r from-pink-500 to-blue-500 "}
-          lefticon={
-            <Image
-              src={"Images/Icon/icon_crab.png"}
-              width={18}
-              height={18}
-              alt="button icon"
-              className="text-white"
-            />
-          }
-        >
-          チケット購入
-        </CommonButton>
-        <EventSummary />
-        <AccessInfo />
-      </main>
+      <EventOverView />
+      <EventSummary />
+      <AccessInfo />
     </div>
   );
 }

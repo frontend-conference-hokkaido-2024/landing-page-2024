1333350921d1e5871d5eae51ead6a25bc6bcd52a
618knot <knot618@gmail.com>
2024-06-09 16:16:49 +0900

    trimしない

--- src/components/Job.tsx
+++ src/components/Job.tsx
@@ -8,30 +8,18 @@ import CommonButton from "./ui/CommonButton";
 
 import type { UrlObject } from "url";
 
-const MAX_JOB_COMMENT_LENGTH = 50;
-
-const trimJobComment = (jobComment: string) => {
-    if (jobComment.length > MAX_JOB_COMMENT_LENGTH) {
-        jobComment = jobComment.substring(0, MAX_JOB_COMMENT_LENGTH) + "...";
-    }
-
-    return jobComment;
-};
-
 type Job = {
-    comment: string; // 50文字まで
+    comment: string; // 50文字程度
     imageUrl: string;
     href: UrlObject | string;
     alt?: string;
 }
 
 export default function Job({ comment, imageUrl, href, alt = "" }: Job) {
-    const trimedJobComment = trimJobComment(comment);
-
     return (
         <div className="w-72 flex flex-col gap-3 p-6 bg-ivory border rounded-md border-black">
             <Image className="w-full h-44 bg-white object-contain rounded-md" src={imageUrl} alt={alt} width={176} height={176} />
-            <Paragraph className="break-all">{trimedJobComment}</Paragraph>
+            <Paragraph className="break-all">{comment}</Paragraph>
             <CommonButton className="drop-shadow-md" asChild>
                 <Link className="w-full" href={href} target="_blank" rel="noopener noreferrer">
                     詳細はこちら

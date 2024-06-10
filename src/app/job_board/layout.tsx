4caa97b2f3f1dbeef61e95f6612c2329b663139a
618knot <knot618@gmail.com>
2024-06-11 00:04:04 +0900

    /job_board追加

---
+++ src/app/job_board/layout.tsx
@@ -0,0 +1,32 @@
+import clsx from "clsx";
+import { Inter, Noto_Sans_JP } from "next/font/google";
+
+import Base from "@/components/Base";
+import Footer from "@/components/Footer";
+
+import "../globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+const NotoSansJP = Noto_Sans_JP({ subsets: ["latin"] });
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="ja" className="scroll-smooth">
+      <body className={clsx("relative")} style={{fontFamily: `${inter.style.fontFamily}, ${NotoSansJP.style.fontFamily}`}}>
+        <div></div>{/*背景に何か要素をつけたいのであれば...*/}
+        <Base>
+          <div className="w-full flex flex-col gap-24">
+            <main className="flex flex-col gap-24 lg:px-12 px-8">
+              {children}
+            </main>
+            <Footer />
+          </div>
+        </Base>
+      </body>
+    </html>
+  );
+}

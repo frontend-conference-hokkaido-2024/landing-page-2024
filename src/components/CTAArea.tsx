e73ff01e5856572c632c593c5594b2b1f9cfad91
Saito Kengo <154238842+kengo2003@users.noreply.github.com>
2024-06-12 00:55:24 +0900

    EventOverViewの修正 (#148)
    * EventOverViewの修正
    
    * 時間の修正
    
    * チケット購入ボタンの設置

--- src/components/CTAArea.tsx
+++ src/components/CTAArea.tsx
@@ -12,13 +12,20 @@ export default function CTAArea() {
         background="bg-gradient-to-r from-pink-500 to-blue-500"
         asChild
       >
-        <Link href="https://example.com" className="w-full" target="_blank" rel="noopener noreferrer">
+        <Link
+          href="https://frontend-conf-2024-hokkaido.peatix.com/"
+          className="w-full"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
           <Ticket />
           <span className="px-2.5">チケット購入</span>
         </Link>
       </CommonButton>
 
-      <small className="text-caution text-xs font-bold">※オンライン視聴の場合もチケット購入が必要です</small>
+      <small className="text-caution text-xs font-bold">
+        ※オンライン視聴の場合もチケット購入が必要です
+      </small>
     </div>
   );
 }

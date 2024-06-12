090fd99b9276c67c9237c310ff883db69e158138
髙橋紀美 <xvipea0@gmail.com>
2024-06-12 22:39:55 +0900

    文字間隔の調整

--- src/components/EventSummary.tsx
+++ src/components/EventSummary.tsx
@@ -1,62 +1,90 @@
+import { Ticket } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
 
-import { Button } from "./ui/button";
+import CommonButton from "./ui/CommonButton";
 
 import frontend_conference_hokkaido_2024_logo from "@/asset/frontend_conference_hokkaido_2024_logo.png";
+import Title from "@/components/elements/Title";
 
 export default function EventSummary() {
   return (
     <div className="mx-5 text-center">
-      <h1 className="text-3xl font-extrabold mb-8">イベント概要</h1>
-      <h3 className="font-semibold text-xl break-keep mb-4">
-        フロントエンドカンファレンス
-        <wbr />
-        北海道2024を開催します！
-      </h3>
-
-      <div className="mb-4">
-        <Image
-          src={frontend_conference_hokkaido_2024_logo}
-          alt=""
-          className=" rounded-md"
-        />
+      <Title
+        icon={
+          <Image
+            src="/images/Icon/icon_bear.png"
+            alt="Icon"
+            className="object-contain"
+            width={65} // 適切な幅を指定
+            height={65} // 適切な高さを指定
+          />
+        }
+      >
+        {"イベント概要"}
+      </Title>
+      <Image
+        src={frontend_conference_hokkaido_2024_logo}
+        alt=""
+        className=" rounded-md"
+      />
+      <div className="mt-4">
+        <h3 className="tracking-wide font-semibold text-xl break-keep">
+          フロントエンドカンファレンス
+          <wbr />
+          北海道2024を開催します！
+        </h3>
       </div>
 
-      <p className="break-words text-left mt-8 mb-8 indent-5">
+      <p className="tracking-wider break-words text-left mt-8 mb-8 indent-5">
         2024年8月24日（土）に北海道札幌市で開催する、フロントエンド領域に関心のある参加者を対象とした技術イベントです。
       </p>
-      <p className="break-words text-left mb-8 indent-5">
+      <p className="tracking-wider break-words text-left mb-8 indent-5">
         2023年11月に沖縄で開催された、フロントエンドカンファレンス沖縄をリスペクトしたカンファレンスでもあります。
       </p>
-      <p className="break-words text-left mb-8 indent-5">
+      <p className="tracking-wider break-words text-left mb-8 indent-5">
         札幌を中心に、全国のプロダクト開発に関わるエンジニアとデザイナーが交わる場を作ることを目指し、HTML、CSS、JavaScript、UI、UX、デザイン、アクセシビリティ、SPA、PWAなど、Webに携わるすべてのフロントエンド領域に関心のある方を対象としています。
       </p>
-      <p className="break-words text-left mb-8 indent-5">
+      <p className="tracking-wider break-words text-left mb-8 indent-5">
         札幌市内での現地開催のほか、オンライン配信（アーカイブあり）も予定しております。
         参加チケットは販売開始次第、公式Xアカウント(@fec_hokkaido)でお知らせします。
         ぜひフォローして、通知をオンにしてお待ちください。
       </p>
 
-      <div className="flex flex-col justify-start mt-5">
-        <Button asChild className="font-bold text-sm mb-4">
-          <Link
-            href="https://twitter.com/fec_hokkaido"
-            target="_blank"
-            rel="noopener noreferrer"
+      <div className="flex flex-col justify-start mt-5 space-y-4">
+        <div className="space-y-1">
+          <CommonButton
+            background="bg-gradient-to-r from-pink-500 to-blue-500"
+            asChild
           >
-            @fec_hokkaido をフォロー
+            <Link
+              href="https://frontend-conf-2024-hokkaido.peatix.com/"
+              className="w-full"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <Ticket />
+              <span className="px-2.5">チケット購入</span>
+            </Link>
+          </CommonButton>
+          <small className="text-caution text-xs font-bold">
+            ※オンライン視聴の場合もチケット購入が必要です
+          </small>
+        </div>
+        <CommonButton asChild>
+          <Link href="https://twitter.com/fec_hokkaido">
+            <span className="px-2.5">@fec_hokkaido をフォロー！</span>
           </Link>
-        </Button>
-        <Button asChild className="font-bold text-sm">
+        </CommonButton>
+        <CommonButton asChild>
           <Link
             href={`https://twitter.com/intent/tweet?hashtags=frontendo&url=${process.env.PRODUCTION_URL}`}
             target="_blank"
             rel="noopener noreferrer"
           >
-            #frontendo でシェアする
+            <span className="px-2.5">#frontendo でシェア！</span>
           </Link>
-        </Button>
+        </CommonButton>
       </div>
     </div>
   );

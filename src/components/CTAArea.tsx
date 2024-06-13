e29f89d714dc51fdad51cd6aa362375bcb541b4a
髙橋紀美 <xvipea0@gmail.com>
2024-06-13 23:30:55 +0900

    チケット購入ボタンの調整

--- src/components/CTAArea.tsx
+++ src/components/CTAArea.tsx
@@ -8,25 +8,26 @@ import CommonButton from "./ui/CommonButton";
 export default function CTAArea() {
   return (
     <div className="flex flex-col gap-1 items-center w-full">
-      <CommonButton
-        background="bg-gradient-to-r from-pink-500 to-blue-500"
-        asChild
-      >
-        <Link
-          href="https://frontend-conf-2024-hokkaido.peatix.com/"
-          className="w-full"
-          target="_blank"
-          rel="noopener noreferrer"
+      <div className="space-y-1">
+        <CommonButton
+          background="bg-gradient-to-r from-pink-500 to-blue-500"
+          asChild
         >
-          
-          <Ticket />
-          <span className="px-2.5">チケット購入</span>
-        </Link>
-      </CommonButton>
+          <Link
+            href="https://frontend-conf-2024-hokkaido.peatix.com/"
+            className="w-full"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <Ticket />
+            <span className="px-2.5">チケット購入</span>
+          </Link>
+        </CommonButton>
 
-      <small className="text-caution text-xs font-bold">
-        ※オンライン視聴の場合もチケット購入が必要です
-      </small>
+        <small className="text-caution text-xs font-bold">
+          ※オンライン視聴の場合もチケット購入が必要です
+        </small>
+      </div>
     </div>
   );
 }

262ccb53600c1038b26165bea7c2ca802a212cbd
618knot <knot618@gmail.com>
2024-06-14 02:57:36 +0900

    コンポーネントの呼び出し

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,10 @@ import AccessInfo from "@/components/AccessInfo";
 import EventOverView from "@/components/EventOverView";
 import EventSummary from "@/components/EventSummary";
 import Online from "@/components/Online";
-
+import CoreStaffSection from "@/components/coreStaffSection/CoreStaffSection";
+import PersonalSponsorSection from "@/components/personalSponsorSection/PersonalSponsorSection";
+import SponsorSectionView from "@/components/sponsorSection/SponsorSectionView";
+import StaffBlog from "@/components/staffBlog/staffBlog";
 
 export default function Home() {
   return (
@@ -11,6 +14,10 @@ export default function Home() {
       <EventSummary />
       <AccessInfo />
       <Online />
+      <StaffBlog />
+      <SponsorSectionView />
+      <PersonalSponsorSection />
+      <CoreStaffSection />
     </div>
   );
 }

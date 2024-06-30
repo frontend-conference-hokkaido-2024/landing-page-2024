516b15a0fc7b6caa2b127ebf29da32b5affaaa9b
髙橋紀美 <xvipea0@gmail.com>
2024-06-30 21:28:34 +0900

    スポンサージョブボード　追加スポンサー分反映

--- src/app/job-board/@jobBoardSection/data/constants.ts
+++ src/app/job-board/@jobBoardSection/data/constants.ts
@@ -3,92 +3,134 @@ import type { JobBoardSectionProps } from "./types";
 export const JOB_BOARD_SECTION_DATA: JobBoardSectionProps = [
   // CodeGrid
   {
-    comment: "Jamstackとエッジサーバーレスの会社のピクセルグリッドでは、エンジニアを募集中",
+    comment:
+      "Jamstackとエッジサーバーレスの会社のピクセルグリッドでは、エンジニアを募集中",
     imageUrl: "images/SponsorSection/platina/logo_PixelGrid.png",
-    href: "https://www.pxgrid.com/careers/"
+    href: "https://www.pxgrid.com/careers/",
   },
 
   // メシウス株式会社
   {
-    comment: "【フルリモート】開発者に力を届ける仲間を募集中！エンジニアの経験を活かせます！",
+    comment:
+      "【フルリモート】開発者に力を届ける仲間を募集中！エンジニアの経験を活かせます！",
     imageUrl: "images/SponsorSection/platina/logo_MESCIUS.png",
-    href: "https://developer.mescius.jp/about-us/recruit"
+    href: "https://developer.mescius.jp/about-us/recruit",
   },
 
   // チームラボ
   {
     comment: "成長と挑戦が待つ。チームラボのフロントエンドエンジニア募集中。",
     imageUrl: "images/SponsorSection/platina/logo_teamLab.png",
-    href: "https://www.team-lab.com/recruit/"
+    href: "https://www.team-lab.com/recruit/",
   },
 
   // 株式会社HAMWORKS
   {
-    comment: "WordPressとJavaScriptの最新技術を共に学び活用するWebエンジニア募集中！",
+    comment:
+      "WordPressとJavaScriptの最新技術を共に学び活用するWebエンジニア募集中！",
     imageUrl: "images/SponsorSection/local-gold/logo_HAMWORKS.png",
-    href: "https://ham.works/careers/"
+    href: "https://ham.works/careers/",
   },
 
   // さくらインターネット株式会社
   {
-    comment: "エンジニア募集中！社会を支えるパブリッククラウドを、全国各地のフロントエンジニアと一緒に作りませんか？",
+    comment:
+      "エンジニア募集中！社会を支えるパブリッククラウドを、全国各地のフロントエンジニアと一緒に作りませんか？",
     imageUrl: "images/SponsorSection/local-gold/logo_sakura.png",
-    href: "https://www.sakura.ad.jp/recruit/"
+    href: "https://www.sakura.ad.jp/recruit/",
   },
 
   // フェンリル株式会社・NILTO
   {
-    comment: "昨年末に公開し急成長中のヘッドレスCMS「NILTO」を開発するプロダクトエンジニア募集中❗️",
+    comment:
+      "昨年末に公開し急成長中のヘッドレスCMS「NILTO」を開発するプロダクトエンジニア募集中❗️",
     imageUrl: "images/SponsorSection/local-gold/logo_NILTO.png",
-    href: "https://hrmos.co/pages/fenrir/jobs/201100"
+    href: "https://hrmos.co/pages/fenrir/jobs/201100",
   },
 
   // 株式会社カオナビ
   {
-    comment: "業界No.1のタレントマネジメントシステムを一緒に開発してくれるエンジニアを募集中！",
+    comment:
+      "業界No.1のタレントマネジメントシステムを一緒に開発してくれるエンジニアを募集中！",
     imageUrl: "images/SponsorSection/gold/logo_kaonavi.png",
-    href: "https://corp.kaonavi.jp/recruit/recruitment/"
+    href: "https://corp.kaonavi.jp/recruit/recruitment/",
   },
 
   // 株式会社LayerX
   {
-    comment: "LayerXでは「お客様に最高の体験を届けたい」フロントエンドエンジニアを募集しています！",
+    comment:
+      "LayerXでは「お客様に最高の体験を届けたい」フロントエンドエンジニアを募集しています！",
     imageUrl: "images/SponsorSection/gold/logo_LayerX.png",
-    href: "https://tech.layerx.co.jp/entry/2023/08/02/180000"
+    href: "https://tech.layerx.co.jp/entry/2023/08/02/180000",
   },
 
   // サイボウズ株式会社
   {
     comment: "サイボウズのフロントエンドを最高にするメンバーを募集しています！",
     imageUrl: "images/SponsorSection/gold/logo_cybozu.png",
-    href: "https://cybozu.co.jp/recruit/"
+    href: "https://cybozu.co.jp/recruit/",
   },
 
   // 株式会社ブルーイッシュ
   {
-    comment: "AIソリューション開発における全ての工程をオールインワンにまとめたAI・LLMアプリ開発プラットフォームのエンジニアを募集中！",
+    comment:
+      "AIソリューション開発における全ての工程をオールインワンにまとめたAI・LLMアプリ開発プラットフォームのエンジニアを募集中！",
     imageUrl: "images/SponsorSection/silver/logo_Blueish.png",
-    href: "https://www.wantedly.com/companies/company_2523491/projects"
+    href: "https://www.wantedly.com/companies/company_2523491/projects",
   },
 
   // Vue Fes Japan
   {
-    comment: "国内最大のVue.jsカンファレンス、Vue Fes Japan 2024は10月19日（土）開催！",
+    comment:
+      "国内最大のVue.jsカンファレンス、Vue Fes Japan 2024は10月19日（土）開催！",
     imageUrl: "images/SponsorSection/silver/logo_VueFesJapan.png",
-    href: "https://vuefes.jp/2024/"
+    href: "https://vuefes.jp/2024/",
   },
 
   // chot Inc.
   {
-    comment: "フロントエンドエンジニア募集中（Next.js/Vercel・リモートOK・フルフレックス）",
+    comment:
+      "フロントエンドエンジニア募集中（Next.js/Vercel・リモートOK・フルフレックス）",
     imageUrl: "images/SponsorSection/silver/logo_chot.png",
-    href: "https://chot-inc.com/recruit"
+    href: "https://chot-inc.com/recruit",
   },
 
   // MIERUNE
   {
-    comment: "オープンソースのコミュニティから生まれた、位置情報技術の職人集団です。カジュアル面談やってます！",
+    comment:
+      "オープンソースのコミュニティから生まれた、位置情報技術の職人集団です。カジュアル面談やってます！",
     imageUrl: "images/SponsorSection/silver/logo_MIERUNE.png",
-    href: "https://www.mierune.co.jp/careers"
+    href: "https://www.mierune.co.jp/careers",
+  },
+
+  // 合同会社DMM.com
+  {
+    comment:
+      "さまざまな事業を展開するDMMで一緒に開発してくれるエンジニアを募集中です！",
+    imageUrl: "images/SponsorSection/silver/logo_DMM.png",
+    href: "https://dmm-corp.com/recruit/search/?tags=184",
+  },
+
+  // LINEヤフー株式会社
+  {
+    comment:
+      "LINEヤフーのプロダクトを、 React や Vue.js などのWeb技術を活用して開発するフロントエンドエンジニアを募集しています！",
+    imageUrl: "images/SponsorSection/silver/logo_LINEYahoo.png",
+    href: "https://www.lycorp.co.jp/ja/recruit/career/job-categories/#j-0-0",
+  },
+
+  // 株式会社ゆめみ
+  // {
+  //   comment:
+  //     "フロントエンドの最新技術に興味があり、",
+  //   imageUrl: "images/SponsorSection/reception/logo_yumemi.png",
+  //   href: "https://hrmos.co/pages/yumemi/jobs?jobType=FULL&category=1507899964010631168",
+  // },
+
+  // 株式会社サイバーエージェント
+  {
+    comment: "業界を牽引するフロントエンドエンジニア募集中",
+    imageUrl: "images/SponsorSection/reception/logo_CyberAgent.png",
+    href: "https://hrmos.co/pages/cyberagent-group/jobs/0000325",
   },
 ];

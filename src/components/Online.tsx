339e46363b162f93fa094596a86e2d2cd1351a49
618knot <knot618@gmail.com>
2024-08-28 01:07:24 +0900

    アーカイブ調整中文言を追加

--- src/components/Online.tsx
+++ src/components/Online.tsx
@@ -7,6 +7,7 @@ export default function Online() {
       <Title icon={<WifiLogo />}>オンライン</Title>
       <div>
         <p className="font-semibold text-2xl text-center">Coming soon...</p>
+        <p className="font-semibold text-sm text-center break-keep">当日のアーカイブは公開に向けて<wbr />現在調整中です。<br />公開まで今しばらくお待ちください。</p>
         {/* YouTube埋め込み */}
         {/* <iframe className="mx-auto" src="https://www.youtube.com/embed/6vib77CUxNM?si=3E1JhDeCHo-gsQEm" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe> */}
       </div>

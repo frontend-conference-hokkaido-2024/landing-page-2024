c5c1f6d6d6ccc8d333f86acab693479d12a2fb3e
618knot <knot618@gmail.com>
2024-10-21 22:27:31 +0900

    アーカイブ表示の準備

--- src/components/Online.tsx
+++ src/components/Online.tsx
@@ -5,11 +5,13 @@ export default function Online() {
   return (
     <section id="streaming">
       <Title icon={<WifiLogo />}>オンライン</Title>
+      <div className="mb-6">
+        <p className="font-semibold text-2xl text-center">#カケハシ</p>
+        <iframe className="aspect-video w-2/3 mx-auto" src="https://www.youtube.com/embed/6vib77CUxNM?si=3E1JhDeCHo-gsQEm" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
+      </div>
       <div>
-        <p className="font-semibold text-2xl text-center">Coming soon...</p>
-        <p className="font-semibold text-sm text-center break-keep">当日のアーカイブは公開に向けて<wbr />現在調整中です。<br />公開まで今しばらくお待ちください。</p>
-        {/* YouTube埋め込み */}
-        {/* <iframe className="mx-auto" src="https://www.youtube.com/embed/6vib77CUxNM?si=3E1JhDeCHo-gsQEm" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe> */}
+        <p className="font-semibold text-2xl text-center">#LINEヤフーWOWルーム</p>
+        <iframe className="aspect-video w-2/3 mx-auto" src="https://www.youtube.com/embed/6vib77CUxNM?si=3E1JhDeCHo-gsQEm" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
       </div>
     </section>
   );

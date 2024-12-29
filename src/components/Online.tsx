b42c2c98b0d8c3f48ac7989628bca27e5ff6ea00
618knot <knot618@gmail.com>
2024-12-30 01:52:48 +0900

    アーカイブ反映

--- src/components/Online.tsx
+++ src/components/Online.tsx
@@ -7,11 +7,11 @@ export default function Online() {
       <Title icon={<WifiLogo />}>オンライン</Title>
       <div className="mb-6">
         <p className="font-semibold text-2xl text-center">#カケハシ</p>
-        <iframe className="aspect-video w-2/3 mx-auto" src="https://www.youtube.com/embed/6vib77CUxNM?si=3E1JhDeCHo-gsQEm" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
+        <iframe className="aspect-video lg:w-4/5 w-full mx-auto" src="https://www.youtube.com/embed/XnLPdrKTOug?si=RKN2WnW8S0DD-hqf" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
       </div>
       <div>
         <p className="font-semibold text-2xl text-center">#LINEヤフーWOWルーム</p>
-        <iframe className="aspect-video w-2/3 mx-auto" src="https://www.youtube.com/embed/6vib77CUxNM?si=3E1JhDeCHo-gsQEm" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
+        <iframe className="aspect-video lg:w-4/5 w-full mx-auto" src="https://www.youtube.com/embed/fpXLHtka86g?si=kil9zMv-jcaF3Me5" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
       </div>
     </section>
   );

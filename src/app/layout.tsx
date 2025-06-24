fb608637ca11b5f5a74f384df7c5d29f69e0ca28
618knot <publicac.clt618knot@gmail.com>
2025-06-24 21:46:39 +0900

    metadata修正

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,15 +7,10 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 const NotoSansJP = Noto_Sans_JP({ subsets: ["latin"] });
 
-const title: string = "フロントエンドカンファレンス北海道2024";
-const description: string =
-  "フロントエンド領域のエンジニア・デザイナー向けの技術カンファレンス「フロントエンドカンファレンス北海道2024」を2024年8月24日(土)に札幌で開催";
-const targetURL = process.env.PRODUCTION_URL_2024;
-if (!targetURL) {
-  throw new Error("PRODUCTION_URL_2024 environment variable is not defined");
-}
+const title: string = "フロントエンドカンファレンス北海道2025";
+const description: string = "フロントエンド領域のエンジニア・デザイナー向けの技術カンファレンス「フロントエンドカンファレンス北海道2025」を2024年9月6日(土)に札幌で開催";
+
 export const metadata: Metadata = {
-  metadataBase: new URL(targetURL),
   title,
   description,
   openGraph: {

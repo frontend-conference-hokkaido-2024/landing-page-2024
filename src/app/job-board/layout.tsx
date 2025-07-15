a8f10c7639027b0e2bc546501c33cb7ad20d805e
kengo2003 <saitokengo2003@gmail.com>
2025-07-15 20:44:32 +0900

    fix: job-boardのlayout修正

--- src/app/job-board/layout.tsx
+++ src/app/job-board/layout.tsx
@@ -1,3 +1,6 @@
+import Base from "@/components/2025/Base";
+import Footer from "@/components/2025/Footer";
+import SpMenu from "@/components/2025/menu/sp/SpMenu";
 
 export default function RootLayout({
   children,
@@ -7,9 +10,17 @@ export default function RootLayout({
   jobBoardSection: React.ReactNode;
 }>) {
   return (
-    <>
-      {children}
-      {jobBoardSection}
-    </>
+    <Base>
+      <SpMenu />
+      <div className="w-full flex flex-col gap-24">
+        <main className="flex flex-col gap-24 lg:px-12 px-8">
+          <div className="w-full flex flex-col gap-24">
+            {children}
+            {jobBoardSection}
+          </div>
+        </main>
+        <Footer />
+      </div>
+    </Base>
   );
 }
